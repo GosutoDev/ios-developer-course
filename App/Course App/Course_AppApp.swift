2052a88f5d8ee1e6a52f2eb86bb17a687186a0e8
GosutoDev <duchoslav.tomas@gmail.com>
2024-05-11 13:07:43 +0200

    [feat] added Firebase swift package and try to setup

--- App/Course App/Course_AppApp.swift
+++ App/Course App/Course_AppApp.swift
@@ -5,14 +5,28 @@
 //  Created by Tomáš Duchoslav on 26.04.2024.
 //
 
+import FirebaseCore
 import SwiftUI
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
 
 @main
 // swiftlint:disable:next type_name
 struct Course_AppApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView {
+                ContentView()
+            }
         }
     }
 }
