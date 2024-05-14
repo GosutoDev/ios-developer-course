c53165fa44c36d765f21a525f45bae4a8f0b8204
GosutoDev <duchoslav.tomas@gmail.com>
2024-05-14 12:06:59 +0200

    [feat] setup collectionView and Delegate

--- App/Course App/Views/UIKit/HomeViewController.swift
+++ App/Course App/Views/UIKit/HomeViewController.swift
@@ -5,23 +5,50 @@
 //  Created by Tomáš Duchoslav on 14.05.2024.
 //
 
+import os
 import SwiftUI
 import UIKit
 
 final class HomeViewController: UIViewController {
+    @IBOutlet private var categoriesCollectionView: UICollectionView!
+    
+    private let logger = Logger()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setup()
     }
-    
-    
-    
 }
 
+extension HomeViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        logger.info("I have tapped \(indexPath)")
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        logger.info("will display \(indexPath)")
+    }
+}
+
+// MARK: - UI SETUP
+private extension HomeViewController {
+    func setup() {
+        setupCollectionView()
+    }
+    func setupCollectionView() {
+        categoriesCollectionView.backgroundColor = UIColor.black
+        categoriesCollectionView.isPagingEnabled = true
+        categoriesCollectionView.showsHorizontalScrollIndicator = false
+        categoriesCollectionView.contentInsetAdjustmentBehavior = .never
+        categoriesCollectionView.delegate = self
+    }
+}
+
+
+// HELPER
 struct HomeView: UIViewControllerRepresentable {
     func makeUIViewController(context: Context) -> HomeViewController {
-        return HomeViewController()
+        HomeViewController()
     }
     
     func updateUIViewController(_ uiViewController: HomeViewController, context: Context) {
