5969862c0d8227b451366656c13b616cc0ad30b3
GosutoDev <duchoslav.tomas@gmail.com>
2024-05-14 12:35:52 +0200

    [feat] created LabelViewCell

---
+++ App/Course App/Views/UIKit/UICollectionViewCells/LabelCollectionViewCell.swift
@@ -0,0 +1,47 @@
+//
+//  LabelCollectionViewCell.swift
+//  Course App
+//
+//  Created by Tomáš Duchoslav on 14.05.2024.
+//
+
+import UIKit
+
+final class LabelCollectionViewCell: UICollectionViewCell {
+    
+    lazy var nameLabel = UILabel()
+ 
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+private extension LabelCollectionViewCell {
+    func setupUI() {
+        addSubview()
+        configureLabel()
+        setupConstraints()
+    }
+    func addSubview() {
+        addSubview(nameLabel)
+    }
+    func configureLabel() {
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        nameLabel.textColor = .white
+    }
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            nameLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 5),
+            nameLabel.topAnchor.constraint(equalTo: topAnchor, constant: 5),
+            nameLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -5),
+            nameLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -5)
+        ])
+    }
+}
