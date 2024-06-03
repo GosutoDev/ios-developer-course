95aed2859f947749488ba32b0b0b6dfe31d8b8be
GosutoDev <duchoslav.tomas@gmail.com>
2024-06-03 11:32:26 +0200

    [feat] create UI for ProfileView

--- App/Course App/Scenes/Profile/ProfileView.swift
+++ App/Course App/Scenes/Profile/ProfileView.swift
@@ -5,14 +5,24 @@
 //  Created by Tomáš Duchoslav on 03.06.2024.
 //
 
+import Combine
 import SwiftUI
 
 struct ProfileView: View {
+    @State private var emailField = ""
+    @State private var passwordField = ""
+    
+    private let eventSubject = PassthroughSubject<ProfileViewEvent, Never>()
+    
     var body: some View {
         VStack {
-            Text("Profile")
-                .foregroundStyle(.black)
-                .textStyle(textType: .baseText)
+            Form {
+                TextField("Email", text: $emailField)
+                SecureField("password", text: $passwordField)
+                Button("SignIn") {
+                    eventSubject.send(.successful)
+                }
+            }
         }
         .navigationTitle("Profile")
     }
---
+++ App/Course App/Scenes/Profile/ProfileViewEvent.swift
@@ -0,0 +1,14 @@
+//
+//  ProfileViewEvent.swift
+//  Course App
+//
+//  Created by Tomáš Duchoslav on 03.06.2024.
+//
+
+import Foundation
+
+enum ProfileViewEvent {
+    case successful
+    case forgotPassword
+    case incorrect
+}
