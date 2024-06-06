edfc379397e6f45739988f238195bebf153c0e4e
GosutoDev <duchoslav.tomas@gmail.com>
2024-06-06 15:51:01 +0200

    [feat] created JokesRouter

---
+++ App/Course App/Networking/Endpoints/JokesRoutes.swift
@@ -0,0 +1,36 @@
+//
+//  JokesRoutes.swift
+//  Course App
+//
+//  Created by Tomáš Duchoslav on 06.06.2024.
+//
+
+import Foundation
+
+enum JokesRoutes: Endpoint {
+    case getJokeCategories
+    case getRandomJoke
+    case getJokeFor(category: String)
+    
+    var host: URL {
+        BuildConfiguration.default.jokesBaseURL
+    }
+    
+    var path: String {
+        switch self {
+        case .getJokeCategories:
+            "jokes/categories"
+        case .getRandomJoke, .getJokeFor:
+            "jokes/random"
+        }
+    }
+    
+    var urlParameters: [String : String] {
+        switch self {
+        case let .getJokeFor(category):
+            ["category": category]
+        default:
+            [String: String]()
+        }
+    }
+}
