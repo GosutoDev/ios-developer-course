3381a429acc117cc8ef4c1656a56c7888f3043dd
GosutoDev <duchoslav.tomas@gmail.com>
2024-06-09 15:21:47 +0200

    [style] added marks and deleted unused code

--- App/Course App/Scenes/Home/HomeViewController.swift
+++ App/Course App/Scenes/Home/HomeViewController.swift
@@ -10,16 +10,6 @@ import os
 import SwiftUI
 import UIKit
 
-// HELPER
-struct HomeView: UIViewControllerRepresentable {
-    func makeUIViewController(context: Context) -> HomeViewController {
-        HomeViewController()
-    }
-    
-    func updateUIViewController(_ uiViewController: HomeViewController, context: Context) {
-    }
-}
-
 final class HomeViewController: UIViewController {
     // MARK: UIConstants
     enum UIConstants {
@@ -29,24 +19,87 @@ final class HomeViewController: UIViewController {
         static let layoutHeaderHeight: CGFloat = 20
     }
     
-    let logger = Logger()
-    
-    lazy var categoriesCollectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createCompositionalLayout())
-    
-    
     // MARK: DataSources
     typealias DataSource = UICollectionViewDiffableDataSource<SectionData, Joke>
     typealias Snapshot = NSDiffableDataSourceSnapshot<SectionData, Joke>
-    lazy var dataSource = makeDataSource()
+    
+    // MARK: Private properties
+    private lazy var dataSource = makeDataSource()
     private var dataProvider = DataProvider()
-    lazy var cancellables = Set<AnyCancellable>()
+    private lazy var cancellables = Set<AnyCancellable>()
     private let jokeService = JokeService(apiManager: APIManager())
+    private lazy var categoriesCollectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createCompositionalLayout())
+    private let logger = Logger()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Categories"
         setup()
+    }
+}
+
+// MARK: - UI SETUP
+private extension HomeViewController {
+    func setup() {
+        setupCollectionView()
+        readData()
         loadData()
+    }
+    func setupCollectionView() {
+        categoriesCollectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        categoriesCollectionView.backgroundColor = .bg
+        categoriesCollectionView.delegate = self
+        view.addSubview(categoriesCollectionView)
+        
+        categoriesCollectionView.register(
+            UICollectionViewCell.self,
+            forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader
+        )
+    }
+}
+
+// MARK: - UICollectionViewDelegate
+extension HomeViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        logger.info("I have tapped \(indexPath)")
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        logger.info("will display \(indexPath)")
+    }
+}
+
+// MARK: - Loading data
+extension HomeViewController {
+    func loadData() {
+        Task {
+            let categories = try await jokeService.loadCategories()
+            
+            try await withThrowingTaskGroup(of: JokeResponse.self) { [weak self] group in
+                guard let self else {
+                    return
+                }
+                
+                for category in categories {
+                    for _ in 1...5 {
+                        group.addTask {
+                            try await self.jokeService.loadJokeForCategory(category)
+                        }
+                    }
+                }
+                
+                var jokeResponses: [JokeResponse] = []
+                for try await jokeResponse in group {
+                    jokeResponses.append(jokeResponse)
+                }
+                
+                let dataDictionary = Dictionary(grouping: jokeResponses, by: { $0.categories.first ?? "" })
+                for key in dataDictionary.keys {
+                    dataProvider.data.append(SectionData(title: key, jokes: dataDictionary[key] ?? []))
+                }
+            }
+        }
+        readData()
     }
 }
 
@@ -109,36 +162,6 @@ private extension HomeViewController {
     }
 }
 
-// MARK: - UICollectionViewDelegate
-extension HomeViewController: UICollectionViewDelegate {
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        logger.info("I have tapped \(indexPath)")
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
-        logger.info("will display \(indexPath)")
-    }
-}
-
-// MARK: - UI SETUP
-private extension HomeViewController {
-    func setup() {
-        setupCollectionView()
-        readData()
-    }
-    func setupCollectionView() {
-        categoriesCollectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        categoriesCollectionView.backgroundColor = .bg
-        categoriesCollectionView.delegate = self
-        view.addSubview(categoriesCollectionView)
-        
-        categoriesCollectionView.register(
-            UICollectionViewCell.self,
-            forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader
-        )
-    }
-}
-
 // MARK: - Layout
 private extension HomeViewController {
     func createCompositionalLayout() -> UICollectionViewLayout {
@@ -163,39 +186,5 @@ private extension HomeViewController {
         layout.configuration = config
         
         return layout
-    }
-}
-
-// MARK: - Loading data
-extension HomeViewController {
-    func loadData() {
-        Task {
-            let categories = try await jokeService.loadCategories()
-            
-            try await withThrowingTaskGroup(of: JokeResponse.self) { [weak self] group in
-                guard let self else {
-                    return
-                }
-                
-                for category in categories {
-                    for _ in 1...5 {
-                        group.addTask {
-                            try await self.jokeService.loadJokeForCategory(category)
-                        }
-                    }
-                }
-                
-                var jokeResponses: [JokeResponse] = []
-                for try await jokeResponse in group {
-                    jokeResponses.append(jokeResponse)
-                }
-                
-                let dataDictionary = Dictionary(grouping: jokeResponses, by: { $0.categories.first ?? "" })
-                for key in dataDictionary.keys {
-                    dataProvider.data.append(SectionData(title: key, jokes: dataDictionary[key] ?? []))
-                }
-            }
-        }
-        readData()
     }
 }
