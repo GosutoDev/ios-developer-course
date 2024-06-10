e63964dc858d019da1494978ee1bd50b58c67c83
GosutoDev <duchoslav.tomas@gmail.com>
2024-06-10 10:11:47 +0200

    [feat] change onFirstAppear instead onAppear in SwipingView

--- App/Course App/Scenes/SwipingView/SwipingView.swift
+++ App/Course App/Scenes/SwipingView/SwipingView.swift
@@ -64,7 +64,7 @@ struct SwipingView: View {
             }
         }
         .navigationTitle("Random")
-        .onAppear {
+        .onFirstAppear() {
             loadRandomJokes()
         }
     }
